9664e27ad2af9e72bc832ffede85c7efc04cf95c
musosy <musosy@users.noreply.github.com>
2021-03-28 19:29:09 +0200

    reset for quest

--- index.php
+++ index.php
@@ -2,23 +2,4 @@
 require_once 'Car.php';
 require_once 'Bicycle.php';
 
-$myCar = new Car('blue', 4, 'gasoil');
-$someoneCar = new Car('red', 4, 'electric');
-$aBike = new Bicycle('pink');
-
-echo $myCar->forward()."<br>";
-echo $myCar->brake()."<br>";
-echo $myCar->start()."<br>";
-echo $myCar->forward()."<br>";
-echo $myCar->forward()."<br>";
-echo $myCar->getCurrentSpeed()."<br>";
-echo $myCar->brake()."<br>";
-echo $myCar->brake()."<br>";
-echo $myCar->brake()."<br>";
-echo $myCar->brake()."<br>";
-echo $myCar->brake()."<br>";
-echo $myCar->brake()."<br>";
-
-echo $someoneCar->getColor()."<br>";
-echo $aBike->getColor();
 ?>
