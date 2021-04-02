f29717a3ea62ff45eefb64fb237af71616d247d8
musosy <musosy@users.noreply.github.com>
2021-04-02 11:06:37 +0200

    added a require file and a skateboard class

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-require_once 'MotorWay.php';
-require_once 'PedestrianWay.php';
-require_once 'ResidentialWay.php';
-require_once 'Car.php';
-require_once 'Bicycle.php';
-require_once 'Truck.php';
-
-
+require_once 'require.php';
 
 $motor = new MotorWay();
 $pedestrian = new PedestrianWay();
@@ -17,6 +10,9 @@ $residential->addVehicle(new Car('blue', 5, 'electric'));
 $motor->addVehicle(new Bicycle('red', 1));
 $pedestrian->addVehicle(new Bicycle('red', 1));
 $residential->addVehicle(new Bicycle('red', 1));
+$motor->addVehicle(new Skateboard('black', 1));
+$pedestrian->addVehicle(new Skateboard('black', 1));
+$residential->addVehicle(new Skateboard('black', 1));
 var_dump($motor);
 var_dump($pedestrian);
 var_dump($residential);
---
+++ require.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'MotorWay.php';
+require_once 'PedestrianWay.php';
+require_once 'ResidentialWay.php';
+
+require_once 'Car.php';
+require_once 'Truck.php';
+require_once 'Bicycle.php';
+require_once 'Skateboard.php';
